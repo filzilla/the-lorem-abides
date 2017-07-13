69227c92d80769ce21514c20557d3817394e5606
filzilla <phillipdelia@Phillips-MacBook-Pro.local>
2017-07-13 16:43:00 -0400

    more boring changes

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ const express = require('express');
 const path = require('path');
 const body_parser = require('body-parser');
 const mustache = require('mustache-express');
-<<<<<<< HEAD
-=======
-
->>>>>>> b86412905e90364d99a3c4f08379f96a40ddd2b0
 const index = require('./routes/index');
 const lebowski_data = require('./lib/data');
 
@@ -13,10 +9,6 @@ const application = express();
 
 // view engine setup
 application.engine('mustache', mustache());
-<<<<<<< HEAD
-
-=======
->>>>>>> b86412905e90364d99a3c4f08379f96a40ddd2b0
 application.set('views', path.join(__dirname, 'views'));
 application.set('view engine', 'mustache');
 
@@ -27,10 +19,7 @@ application.use(express.static(path.join(__dirname, 'public')));
 application.use('/', index);
 
 // misc middleware
-<<<<<<< HEAD
-=======
 application.use(body_parser.json());
->>>>>>> b86412905e90364d99a3c4f08379f96a40ddd2b0
 application.use(body_parser.urlencoded({ extended: false }));
 
 // error handling middleware
@@ -51,36 +40,3 @@ application.use((error, request, response, next) => {
 application.listen(3000, () => {
     console.log('Listening on 3000')
 });
-<<<<<<< HEAD
-
-
-/******DATA******/
-
-// quote arrays
-let lebowski = lebowski_data.lebowski;
-let the_dude = lebowski_data.r_rated;
-
-//general functions
-function populate_ipsum(amount, lebowski) {
-    if(!amount){return lebowski[0];}
-    console.log('times to populate');
-    return populate_ipsum(amount-1, lebowski) + ' ' + lebowski[amount];
-};
-
-function shuffle_array(lebowski_array){
-    let lebowski_string = '';
-    for(var i in lebowski_array){
-        Math.round(Math.random()) ? 
-            lebowski_string = lebowski_array[i] + ' ' + lebowski_string :
-            lebowski_string = lebowski_string + ' ' + lebowski_array[i];
-    }
-    return lebowski_string.split(' ');
-}
-
-//V2 ideas
-//bowling pins sound when page loads
-//R rated version
-//every n(user input variable) letter in the ipsum
-//
-=======
->>>>>>> b86412905e90364d99a3c4f08379f96a40ddd2b0
